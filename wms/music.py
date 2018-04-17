ea50ba18ffb97dad756797ee326d56e499bf8e54
William Neild <wneild5@gmail.com>
2018-04-17 11:55:29 +0000

    allow m4a files to be added to the datatbase

--- wms/music.py
+++ wms/music.py
@@ -1,13 +1,13 @@
 import os, logging
 
 class Search:
-    def __init__(self, paths, supportedExt=[".mp3", ".wav", ".ogg"]):
+    def __init__(self, paths, supportedExt=[".mp3", ".wav", ".ogg", ".m4a"]):
         paths = paths[0].split(",")
         songs = []
         for path in paths:
             for dir, subdir, files in os.walk(path):
                 for file in files:
                     for ext in supportedExt:
-                        if file.endswith(ext):
+                        if file.lower().endswith(ext):
                             songs.append(os.path.join(dir,file))
         self.songs = songs
