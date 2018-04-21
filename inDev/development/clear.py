71bb4959df614b829918d2bd70e0591e4aaca642
William Neild <wneild5@gmail.com>
2018-04-21 20:10:25 +0100

    fixed clear file

--- inDev/development/clear.py
+++ inDev/development/clear.py
@@ -1,12 +1,12 @@
 import os
 
-filesToDelete = ["../../WMS-Core/database/main.db",
-                 "../../WMS-Core/database/users.db",
-                 "../../WMS-Core/database/libraries/music.db",
-                 "../../WMS-Core/database/libraries/films.db",
-                 "../../WMS-Core/database/libraries/tv.db",
-                 "../../WMS-Core/logs/main.log",
-                 "../../WMS-Core/config.ini"]
+filesToDelete = ["../../database/main.db",
+                 "../../database/users.db",
+                 "../../database/libraries/music.db",
+                 "../../database/libraries/films.db",
+                 "../../database/libraries/tv.db",
+                 "../../logs/main.log",
+                 "../../config.ini"]
 
 for f in filesToDelete:
     if os.path.isfile(f):
