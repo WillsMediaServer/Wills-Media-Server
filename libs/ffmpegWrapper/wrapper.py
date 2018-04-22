97a1f02526fa842f7dce5332b4d25f0f64a52d1e
William Neild <wneild5@gmail.com>
2018-04-22 12:06:47 +0100

    added check for either local, systemwide or no ffmpeg install

--- libs/ffmpegWrapper/wrapper.py
+++ libs/ffmpegWrapper/wrapper.py
@@ -1,15 +1,28 @@
-import subprocess
-import json
+import subprocess, json, os, logging
 
 from os.path import join, dirname, abspath, normpath
 LIB_DIR = normpath(join(dirname(abspath(__file__)), ".."))
 
 class Wrapper:
     def __init__(self):
-        pass
+        try:
+            logging.info("Attempting to use ffmpeg from path")
+            data = subprocess.Popen(["ffmpeg", "-version"], stdout=subprocess.PIPE)
+            stdout, stderr = data.communicate()
+            logging.info(stdout.decode("utf-8"))
+            self.ffmpegdir = ""
+        except Exception:
+            try:
+                logging.info("Attempting to use ffmpeg from libs directory")
+                data = subprocess.Popen([join(LIB_DIR, "ffmpeg", "bin", "ffmpeg"), "-version"], stdout=subprocess.PIPE)
+                stdout, stderr = data.communicate()
+                logging.info(stdout.decode("utf-8"))
+                self.ffmpegdir = join(LIB_DIR, "ffmpeg", "bin")
+            except Exception:
+                logging.error("Please add ffmpeg to your path or to the WMS libs directory")
 
     def metadata(self, filename, tags):
-        metadata = subprocess.Popen([join(LIB_DIR, "ffmpeg", "bin", "ffprobe"), "-v", "quiet", "-print_format", "json" ,"-show_format", filename], stdout=subprocess.PIPE)
+        metadata = subprocess.Popen([join(self.ffmpegdir, "ffprobe"), "-v", "quiet", "-print_format", "json" ,"-show_format", filename], stdout=subprocess.PIPE)
         stdout, stderr = metadata.communicate()
         jsonMetadata = json.loads(stdout)
         # print(json.dumps(jsonMetadata))
@@ -20,4 +33,4 @@ class Wrapper:
                 returnData[tag] = neededMetadata["tags"][tag]
             except:
                 returnData[tag] = None
-        print(json.dumps(returnData))
+        return json.dumps(returnData)
