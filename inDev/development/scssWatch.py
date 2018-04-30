23b68ccdf284231dd9c1f5519be05b717cf35aed
William Neild <wneild5@gmail.com>
2018-04-30 01:32:16 +0000

    removed everything to start afresh

--- inDev/development/scssWatch.py
+++
@@ -1,9 +0,0 @@
-# This requires the sass installed
-from os.path import join, dirname, abspath, normpath
-from os import system
-
-BASE_DIR = normpath(join(dirname(abspath(__file__)), "..", ".."))
-inputDir = join(BASE_DIR, "wms", "static", "scss")
-outputDir = join(BASE_DIR, "wms", "static", "css")
-
-system("sass --watch '" + join(inputDir, "main.scss") + "':'" + join(outputDir, "main-dev.css") + "' --style compressed")
