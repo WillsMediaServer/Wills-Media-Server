fa4a1adff12e5a9f400c898badf8e644ed4c7465
William Neild <wneild5@gmail.com>
2018-05-04 21:34:19 +0100

    added media searcher

---
+++ libraries/mediaSearcher/__init__.py
@@ -0,0 +1,48 @@
+#
+# /__init__.py
+# Wills Media Server Media Searcher
+# Version: 0.0.1.0 Alpha
+# Created By William Neild
+#
+
+import logging, os
+
+supportedExtensions = {
+    "music": ["mp3", "wav", "ogg", "m4a", "flac", "aac"],
+    "films": [],
+    "tv": []
+}
+
+class Searcher:
+    def __init__(self, mediaType, paths=[]):
+        self.logger = logging.getLogger('wms.media-searcher')
+        self.mediaResult = []
+        if mediaType == "music":
+            self.mediaResult = self.music(paths)
+        elif mediaType == "films":
+            self.mediaResult = self.films(paths)
+        elif mediaType == "tv":
+            self.mediaResult = self.tv(paths)
+        else:
+            self.logger.error("Media Type doesn't exist")
+
+        if self.mediaResult == []:
+            logger.error("Media Searcher found no supported media files")
+
+
+    def music(self, paths):
+        songs = []
+        for path in paths:
+            for dir, subdir, files in os.walk(path):
+                for filename in files:
+                    for extension in supportedExtensions["music"]:
+                        if filename.lower().endswith(extension):
+                            songs.append(os.path.join(dir, filename))
+        return songs
+
+
+    def films(self, paths):
+        pass
+
+    def tv(self, paths):
+        pass
