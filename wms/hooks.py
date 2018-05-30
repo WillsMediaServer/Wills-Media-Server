91a6a06d46246793473534a94fbc685bce2082f4
William Neild <wneild5@gmail.com>
2018-05-30 20:02:11 +0100

    gzip all flask responses

--- wms/hooks.py
+++ wms/hooks.py
@@ -7,8 +7,12 @@
 #
 
 import logging
+import gzip
+
+from io import BytesIO as IO
 
 from flask import Flask, request
+
 
 
 class Hooks:
@@ -23,9 +27,30 @@ class Hooks:
 
         @app.after_request
         def after_request(response):
+            # Custom logging
             responseLogFormat = "{} {} {} \"{}\" {}".format(
                 request.remote_addr, request.method, response.status, response.mimetype, request.path)
             self.serverLogger.info(responseLogFormat)
+            # Custom headers
             response.headers["Server"] = "Wills Media Server v0.0.1"
             response.headers["Access-Control-Allow-Origin"] = "*"
+            # GZip all requests
+            accept_encoding = request.headers.get('Accept-Encoding', '')
+            if 'gzip' not in accept_encoding.lower():
+                pass
+            else:
+                response.direct_passthrough = False
+
+                if (response.status_code < 200 or response.status_code >= 300 or 'Content-Encoding' in response.headers):
+                    return response
+                gzip_buffer = IO()
+                gzip_file = gzip.GzipFile(mode='wb', fileobj=gzip_buffer)
+                gzip_file.write(response.data)
+                gzip_file.close()
+
+                response.data = gzip_buffer.getvalue()
+                response.headers['Content-Encoding'] = 'gzip'
+                response.headers['Vary'] = 'Accept-Encoding'
+                response.headers['Content-Length'] = len(response.data)
+
             return response
